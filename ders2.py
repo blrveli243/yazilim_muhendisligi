6dd470bcc07fb3abcf6ec9df16c5e226c841ab00
Veli Bilir <velibilir950@gmail.com>
2025-03-27 08:44:17 +0300

    4.gün

--- ders2.py
+++ ders2.py
@@ -20,3 +20,14 @@ print(boy)
 
 boykilo=veriler[['boy','kilo']]
 print(boykilo)
+
+#eksik veriler
+from sklearn.impute import SimpleImputer
+
+imputer= SimpleImputer(missing_values=np.nan,strategy='mean')
+Yas=veriler.iloc[:,1:4].values
+print(Yas)
+
+imputer = imputer.fit(Yas[:,1:4])
+Yas[:,1:4]=imputer.transform(Yas[:,1:4])
+print(Yas)
